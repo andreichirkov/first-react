80713f3ee05bd0b7e010c0b061dba2c9d7810d8d
Andrei Chirkov <aachirkov@me.com>
2022-09-05 16:48:23 +0300

    Refactor rating

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,5 +1,5 @@
 import { RatingProps } from "./Rating.props"
-import {useEffect, useState} from "react"
+import { useEffect, useState, KeyboardEvent } from "react"
 import StarIcon from "./star.svg"
 import cn from "classnames"
 import styles from "./Rating.module.css"
@@ -29,15 +29,21 @@ export const Rating = ({
     //Делаем массив звездочек, заливаем fill
     const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
       return (
-        <StarIcon
+        <span
           className={cn(styles.star, {
             [styles.filled]: i < currentRating,
             [styles.editable]: isEditable
           })}
           onMouseEnter={() => changeDisplay(i + 1)}
           onMouseLeave={() => changeDisplay(rating)}
-          onClick={() => onClick(i + 1)}
-        />
+          onClick={() => onClick(i + 1)}>
+          <StarIcon
+            tabIndex={isEditable ? 0 : -1}
+            onKeyDown={(e: KeyboardEvent<SVGAElement>) =>
+              isEditable && handleSpace(i + 1, e)
+            }
+          />
+        </span>
       )
     })
     setRatingArray(updatedArray)
@@ -52,6 +58,13 @@ export const Rating = ({
 
   const onClick = (i: number) => {
     if (!isEditable || !setRating) {
+      return
+    }
+    setRating(i)
+  }
+
+  const handleSpace = (i: number, e: KeyboardEvent<SVGAElement>) => {
+    if (e.code != "Space" || !setRating) {
       return
     }
     setRating(i)
