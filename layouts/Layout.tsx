76482398463de037021fed5d8c155279334ab0aa
Andrei Chirkov <aachirkov@me.com>
2022-09-06 13:51:20 +0300

    Сss layout

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -8,21 +8,19 @@ import { FunctionComponent } from "react"
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   )
 }
 
 export const withLayout = <T extends Record<string, unknown>>(
   Component: FunctionComponent<T>
 ) => {
-  return function withLayoutComponent(props: T):JSX.Element {
+  return function withLayoutComponent(props: T): JSX.Element {
     return (
       <Layout>
         <Component {...props} />
