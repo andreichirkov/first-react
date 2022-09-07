a9963dd0e43d3ae99497cff15cbf179a10239f79
Andrei Chirkov <aachirkov@me.com>
2022-09-07 12:56:31 +0300

    Refactor grid

--- components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { FooterProps } from "./Footer.props"
-import styles from "./Footer.module.css"
-import cn from "classnames"
-
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>
-}
---
+++ layouts/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { FooterProps } from "./Footer.props"
+import styles from "./Footer.module.css"
+import cn from "classnames"
+
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return <footer className={cn(className, styles.footer)} {...props}>
+      dfbdfbdfbdfb
+  </footer>
+}
