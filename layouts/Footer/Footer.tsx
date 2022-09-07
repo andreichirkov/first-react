87ca63bc9b5c6962796075b59756c9cb8ee0411a
Andrei Chirkov <aachirkov@me.com>
2022-09-07 13:17:03 +0300

    Add date-fns + footer css

--- layouts/Footer/Footer.tsx
+++ layouts/Footer/Footer.tsx
@@ -1,9 +1,14 @@
-import { FooterProps } from "./Footer.props"
+import {FooterProps} from "./Footer.props"
 import styles from "./Footer.module.css"
 import cn from "classnames"
+import {format} from 'date-fns'
 
-export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
   return <footer className={cn(className, styles.footer)} {...props}>
-      dfbdfbdfbdfb
+    <div>
+      OwlTop © 2020 - {format(new Date, 'yyyy')} Все права защищены
+    </div>
+    <a href="#" target='_blank'>Пользовательское соглашение</a>
+    <a href="#" target='_blank'>Политика конфиденциальности</a>
   </footer>
 }
