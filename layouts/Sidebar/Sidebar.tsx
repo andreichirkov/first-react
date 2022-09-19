390a9a8af2568f91f51318a58e963779041c4c7d
Andrei Chirkov <aachirkov@me.com>
2022-09-19 10:48:57 +0300

    Add sidebar grid

--- layouts/Sidebar/Sidebar.tsx
+++ layouts/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import { SidebarProps } from "./Sidebar.props"
 import styles from "./Sidebar.module.css"
 import cn from "classnames"
-import {Menu} from "../Menu/Menu";
+import { Menu } from "../Menu/Menu"
+import Logo from "../logo.svg"
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>
-    <Menu></Menu>
-  </div>
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo}></Logo>
+      <div>Поиск</div>
+      <Menu></Menu>
+    </div>
+  )
 }
