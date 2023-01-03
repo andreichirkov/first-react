7f4328e14b7b471328f87b4edb5a42ef5515bf90
Andrei Chirkov <aachirkov@me.com>
2023-01-03 19:56:57 +0300

    Fix if

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -3,6 +3,7 @@ import cn from "classnames"
 import styles from "./TopPageComponent.module.css"
 import { TopPageComponentProps } from "./TopPageComponent.props"
 import { Card, HhData, Htag, Tag } from "../../components"
+import { TopLevelCategory } from "../../interfaces/page.interface"
 
 export const TopPageComponent = ({
   page,
@@ -13,19 +14,23 @@ export const TopPageComponent = ({
     <div className={styles.wrapper}>
       <div className={styles.title}>
         <Htag tag="h1">{page.title}</Htag>
-        {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
         <span>Сортировка</span>
       </div>
       <div>
-        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+        {products && products.map(p => <div key={p._id}>{p.title}</div>)}
       </div>
       <div className={styles.hhTitle}>
         <Htag tag="h2">Вакансии - {page.category}</Htag>
-        <Tag color='red' size='m'>hh.ru</Tag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
       </div>
-      <HhData {...page.hh} />
-
-
+      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
     </div>
   )
 }
