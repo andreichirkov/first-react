ba00125adc136f2a6e446fcef782b5693ace54fb
Andrei Chirkov <aachirkov@me.com>
2023-01-04 19:39:23 +0300

    Add html for product header

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,6 +1,9 @@
 import { ProductProps } from "./Product.props"
 import styles from "./Product.module.css"
 import cn from "classnames"
+import { Card } from "../Card/Card"
+import { Rating } from "../Rating/Rating"
+import { Tag } from "../Tag/Tag"
 
 export const Product = ({
   product,
@@ -8,8 +11,26 @@ export const Product = ({
   ...props
 }: ProductProps): JSX.Element => {
   return (
-    <div>
-      {product.title}
-    </div>
+    <Card className={styles.product}>
+      <div className={styles.logo}>
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div className={styles.title}>{product.title}</div>
+      <div className={styles.price}>{product.price}</div>
+      <div className={styles.credit}>{product.credit}</div>
+      <div className={styles.rating}>
+        <Rating rating={product.reviewAvg ?? product.initialRating} />
+      </div>
+      <div className={styles.tags}>
+        {product.categories.map(c => (
+          <Tag color={"ghost"} key={c}>
+            {c}
+          </Tag>
+        ))}
+      </div>
+      <div className={styles.priceTitle}>Цена</div>
+      <div className={styles.creditTitle}>Кредит</div>
+      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+    </Card>
   )
 }
